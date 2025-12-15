7f808c20d9a03b720a4f6c9816c212c591109a97
hannahsmith3141 <66369500+hannahsmith3141@users.noreply.github.com>
2025-12-15 10:11:48 +0000

    Delete src/models_library directory

--- src/models_library/difrancesco/build.sh
+++
@@ -1,7 +0,0 @@
-############## DI & FRANCESCO 1985 ##############################
-MODEL_FILE_CPU="difrancesco.c"
-MODEL_FILE_GPU="difrancesco.cu"
-COMMON_HEADERS="difrancesco.h"
-
-COMPILE_MODEL_LIB "difrancesco" "$MODEL_FILE_CPU" "$MODEL_FILE_GPU" "$COMMON_HEADERS"
-#########################################################
--- src/models_library/set_double_precision.h
+++
@@ -1,3 +0,0 @@
-#define REAL_MIN DBL_MIN
-#define REAL_MAX DBL_MAX
-#define REAL_EPS DBL_EPSILON
