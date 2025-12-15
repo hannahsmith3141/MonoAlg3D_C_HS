224c31f3f78a883c7ad56e20db9c2bdc3b2551f4
hannahsmith3141 <66369500+hannahsmith3141@users.noreply.github.com>
2025-12-15 10:11:09 +0000

    Delete src/extra_data_library directory

--- src/extra_data_library/build.sh
+++
@@ -1,5 +0,0 @@
-LIB_STATIC_LIBS="config_helpers utils tinyexpr sds"
-
-CHECK_CUSTOM_FILE
-
-COMPILE_SHARED_LIB "default_extra_data" "extra_data.c helper_functions.c ${CUSTOM_FILE}" "helper_functions.h" "${LIB_STATIC_LIBS}"
